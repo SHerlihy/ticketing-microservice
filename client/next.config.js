68fb27dddc46a1bdf93578e5b39d591af2b2bb81
SHerlihy <steven_herlihy@yahoo.com>
2023-04-07 17:07:16 +0100

    csp header aded to next config

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,29 @@
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' https://connect-js.stripe.com https://js.stripe.com;
+  frame-src https://connect-js.stripe.com;
+  style-src 'self' 'unsafe-unline';
+  font-src 'self';  
+`
+const CSPSecurityHeader = {
+  key: 'Content-Security-Policy',
+  value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
+}
+
+const securityHeaders = [CSPSecurityHeader]
+
 module.exports = {
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ]
+  },
   webpackDevMiddleware: (config) => {
     config.watchOptions.poll = 300;
     return config;
   },
-};
+}
